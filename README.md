7e54587ccaa3dffa9b9b2eadb3a6576ada76d51d
deane <deane@lwork.com>
2017-06-28 17:36:04 +0800

    eureka-register eureka-service

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 "# springcloud-learn" 
+echo "# springcloud-learn" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/djmpink/springcloud-learn.git
+git push -u origin master
--- euraka-service/src/main/java/cn/no7player/EurakaServiceApplication.java
+++ euraka-service/src/main/java/cn/no7player/EurakaServiceApplication.java
@@ -2,13 +2,13 @@ package cn.no7player;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class EurekaRegisterApplication {
+public class EurakaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaRegisterApplication.class, args);
+		SpringApplication.run(EurakaServiceApplication.class, args);
 	}
 }
